3dcd52bf8e35031e59f5faca05ab078ca5df360d
Manu Cabello <Manu Cabello@DESKTOP-I8RV2MI.home>
2019-11-09 01:46:36 +0100

    Métodos funcionales del ejercicio 3 (sin terminar) y del ejercicio 4

--- PI1_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio3.java
+++ PI1_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio3.java
@@ -1,8 +1,14 @@
 package ejercicios;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import us.lsi.common.Files2;
+import us.lsi.common.Pair;
+import us.lsi.geometria.Punto2D;
 
 public class Ejercicio3 {
 	
@@ -44,7 +50,27 @@ public class Ejercicio3 {
 			return ejercicio3_recur_no_final(cadena, i+1, j-1) && cadena.charAt(i) == cadena.charAt(j); 
 		}
 	}
+	
+	// Solución funcional
+	public static boolean ejercicio3_funcional(String cadena) {
+		Pair<Integer,Integer> seed = Pair.of(0, cadena.length()-1);
+		Predicate<Pair<Integer,Integer>> pred = new Predicate<Pair<Integer,Integer>>() {
+			public boolean test(Pair<Integer,Integer> p) {
+				return p.a == p.b;
+			}
+		};
+		UnaryOperator<Pair<Integer,Integer>> xor = aux -> Pair.of(aux.a+1, aux.b-1);
 
+		Predicate<Pair<Integer,Integer>> pred2 = new Predicate<Pair<Integer,Integer>>() {
+			public boolean test(Pair<Integer,Integer> p) {
+				return cadena.charAt(p.a) == cadena.charAt(p.b);
+			}
+		};
+		
+		return Stream.iterate(seed, pred, xor).allMatch(pred2);
+	}
+
+	// #######################################################################################################################
 	// Resultados
 	public static void main(String[] args) {
 		List<String> lineas = Files2.getLines("ficheros/PI1Ej3DatosEntrada.txt");
@@ -76,6 +102,18 @@ public class Ejercicio3 {
 		for (String linea: lineas) {
 			int i = 0; int j = linea.length()-1;
 			boolean res = ejercicio3_recur_no_final(linea,i,j);
+			if (res) {
+				cont++;
+			}
+			System.out.println("esPalindromoIterativa("+linea+")="+res);
+		}
+		System.out.println(cont+" palíndromos de "+lineas.size()+" palabras\n");
+		
+		// Resultado funcional
+		cont = 0;
+		for (String linea: lineas) {
+			int i = 0; int j = linea.length()-1;
+			boolean res = ejercicio3_funcional(linea);
 			if (res) {
 				cont++;
 			}
--- PI1_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio5.java
+++ PI1_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio5.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import us.lsi.common.Files2;
 
+// TODO Cambiar los List<Integer> por el tipo Tuple
 public class Ejercicio5 {
 	
+	// Solución iterativa
 	public static List<Integer> ejercicio5_itera(int a, int b) {
 		List<Integer> res = new ArrayList<Integer>();
 		res.add(0);
@@ -21,35 +23,43 @@ public class Ejercicio5 {
 		return res;
 	}
 	
-	public static List<Integer> ejercicio4_recur_final(int a, int b, List<Integer> res) {
+	// Solución recursiva final
+	public static List<Integer> ejercicio5_recur_final(int a, int b, List<Integer> res) {
 		if (a < b) {
 			return res;
 		} else {
 			res.set(0,res.get(0)+1);
 			res.set(1, a-b);
-			return ejercicio4_recur_final(res.get(1),b,res);
+			return ejercicio5_recur_final(res.get(1),b,res);
 		}
 	}
 	
-	public static List<Integer> ejercicio4_recur_final_gen(int a, int b) {
+	public static List<Integer> ejercicio5_recur_final_gen(int a, int b) {
 		List<Integer> res = new ArrayList<Integer>();
 		res.add(0);
 		res.add(a);
-		return ejercicio4_recur_final(a,b,res);
+		return ejercicio5_recur_final(a,b,res);
 	}
 	
-	public static List<Integer> ejercicio4_recur_no_final(int a, int b) {
+	// Solución recursiva no final
+	public static List<Integer> ejercicio5_recur_no_final(int a, int b) {
 		List<Integer> res = new ArrayList<Integer>();
 		if (a < b) {
 			res.add(0);
 			res.add(a);
 		} else {
-			res.add(ejercicio4_recur_no_final(a-b,b).get(0)+1);
-			res.add(ejercicio4_recur_no_final(a-b,b).get(1));
+			res.add(ejercicio5_recur_no_final(a-b,b).get(0)+1);
+			res.add(ejercicio5_recur_no_final(a-b,b).get(1));
 		}
 		return res;
 	}
 	
+	// Solución funcional
+	// Stream.iterate({a,c}, cond.continuidad, op.unario) -> op.unario puede ser función auxiliar t->siguiente(t)
+	
+	
+	// #######################################################################################################################
+	// Resultados
 	public static void main(String[] args) {
 		List<String> lineas = Files2.getLines("ficheros/PI1Ej5DatosEntrada.txt");
 		
@@ -70,7 +80,7 @@ public class Ejercicio5 {
 			String[] linea_array = linea.split(",");
 			int a = Integer.parseInt(linea_array[0]);
 			int b = Integer.parseInt(linea_array[1]);
-			List<Integer> res = ejercicio4_recur_final_gen(a,b);
+			List<Integer> res = ejercicio5_recur_final_gen(a,b);
 			System.out.println("Entrada: ("+a+","+b+")");
 			System.out.println("Salida:  ("+res.get(0)+","+res.get(1)+")");
 			System.out.println("========================================");
@@ -82,7 +92,7 @@ public class Ejercicio5 {
 			String[] linea_array = linea.split(",");
 			int a = Integer.parseInt(linea_array[0]);
 			int b = Integer.parseInt(linea_array[1]);
-			List<Integer> res = ejercicio4_recur_no_final(a,b);
+			List<Integer> res = ejercicio5_recur_no_final(a,b);
 			System.out.println("Entrada: ("+a+","+b+")");
 			System.out.println("Salida:  ("+res.get(0)+","+res.get(1)+")");
 			System.out.println("========================================");
