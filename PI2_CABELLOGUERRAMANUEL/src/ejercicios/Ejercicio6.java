d16506a8e416ca5f231f9d8b6f3cc0d02b3f39f8
Manu Cabello <manu@manusoft.es>
2019-12-16 16:00:42 +0100

    Creación de ficheros de PI2A y comienzo del ejercicio 6

---
+++ PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio6.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+import java.util.function.Function;
+
+import us.lsi.tiposrecursivos.BinaryTree;
+
+public class Ejercicio6 {
+	
+	public static <E,R> BinaryTree<R> ejercicio6(BinaryTree<E> tree, Function<E,R> function) {
+		BinaryTree<R> res = BinaryTree.empty();
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
