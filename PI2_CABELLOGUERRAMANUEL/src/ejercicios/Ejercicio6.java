88b0dd10b125a5cdbfc0cb6c96b89d85657fb599
Manu Cabello <manu@manusoft.es>
2019-12-18 20:22:41 +0100

    Terminado ejercicio 6 completo

--- PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio6.java
+++ PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio6.java
@@ -1,10 +1,12 @@
 package ejercicios;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 import us.lsi.common.Files2;
 import us.lsi.tiposrecursivos.BinaryTree;
+import us.lsi.tiposrecursivos.Tree;
 
 public class Ejercicio6 {
 	
@@ -12,8 +14,23 @@ public class Ejercicio6 {
 		BinaryTree<R> res = BinaryTree.empty();
 		if (!tree.isEmpty() && tree.isLeaf()) {
 			res = BinaryTree.leaf(function.apply(tree.getLabel()));
-		} else if(!tree.isEmpty()) {
+		} else if (!tree.isEmpty()) {
 			res =  BinaryTree.binary(function.apply(tree.getLabel()),ejercicio6_binario(tree.getLeft(),function),ejercicio6_binario(tree.getRight(),function));
+		}
+		return res;
+	}
+	
+	public static <E,R> Tree<R> ejercicio6_nario(Tree<E> tree, Function<E,R> function) {
+		Tree<R> res = Tree.empty();
+		if (!tree.isEmpty() && tree.isLeaf()) {
+			res = Tree.leaf(function.apply(tree.getLabel()));
+		} else if (!tree.isEmpty()) {
+			List<Tree<E>> children_e = tree.getChildren();
+			List<Tree<R>> children_r = new ArrayList<Tree<R>>();
+			for (Tree<E> child_e : children_e) {
+				children_r.add(ejercicio6_nario(child_e,function));
+			}
+			res = Tree.nary(function.apply(tree.getLabel()), children_r);
 		}
 		return res;
 	}
@@ -26,13 +43,26 @@ public class Ejercicio6 {
 		System.out.println("          TEST DEL EJERCICIO 6 PARA ARBOLES BINARIOS");
 		System.out.println("==============================================================");
 		
-		List<String> lineas = Files2.getLines("ficheros/PI2Ej6DatosEntradaArbolBinario.txt");
-		for (String linea: lineas) {
+		List<String> lineas_b = Files2.getLines("ficheros/PI2Ej6DatosEntradaArbolBinario.txt");
+		for (String linea: lineas_b) {
 			System.out.println("Entrada: "+linea);
 			BinaryTree<String> tree = BinaryTree.parse(linea);
-			System.out.println("		Salida1: "+ejercicio6_binario(tree,function1).toString());
-			System.out.println("		Salida2: "+ejercicio6_binario(tree,function2).toString());
-			System.out.println("		Salida3: "+ejercicio6_binario(tree,function3).toString());
+			System.out.println("	Salida1: "+ejercicio6_binario(tree,function1).toString());
+			System.out.println("	Salida2: "+ejercicio6_binario(tree,function2).toString());
+			System.out.println("	Salida3: "+ejercicio6_binario(tree,function3).toString());
+			System.out.println("--------------------------------------------------------------");
+		}
+		
+		System.out.println("\n          TEST DEL EJERCICIO 6 PARA ARBOLES N-ARIOS");
+		System.out.println("==============================================================");
+		
+		List<String> lineas_n = Files2.getLines("ficheros/PI2Ej6DatosEntradaArbolNario.txt");
+		for (String linea: lineas_n) {
+			System.out.println("Entrada: "+linea);
+			Tree<String> tree = Tree.parse(linea);
+			System.out.println("	Salida1: "+ejercicio6_nario(tree,function1).toString());
+			System.out.println("	Salida2: "+ejercicio6_nario(tree,function2).toString());
+			System.out.println("	Salida3: "+ejercicio6_nario(tree,function3).toString());
 			System.out.println("--------------------------------------------------------------");
 		}
 	}
