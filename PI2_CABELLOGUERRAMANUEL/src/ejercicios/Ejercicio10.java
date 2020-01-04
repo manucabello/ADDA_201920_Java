a637356cf8a6a57cc80f31a6e00ee3746c20d010
Manu Cabello <manu@manusoft.es>
2020-01-04 02:21:42 +0100

    Ejercicio 9 y 10 resueltos

--- PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio10.java
+++ PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio10.java
@@ -1,10 +1,42 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import us.lsi.common.Files2;
+import us.lsi.tiposrecursivos.BinaryTree;
+
 public class Ejercicio10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static List<String> ejercicio10_gen(BinaryTree<String> tree, String cadena, List<String> res) {
+		if (tree.getLabel().compareTo(cadena) >= 0 && tree.isLeaf()) {
+			res.add(tree.getLabel());
+		} else if (tree.getLabel().compareTo(cadena) < 0 && tree.isBinary()) {
+			ejercicio10_gen(tree.getLeft(),cadena,res);
+			ejercicio10_gen(tree.getRight(),cadena,res);
+		} else if (tree.getLabel().compareTo(cadena) >= 0 && tree.isBinary()) {
+			res.add(tree.getLabel());
+			ejercicio10_gen(tree.getLeft(),cadena,res);
+			ejercicio10_gen(tree.getRight(),cadena,res);
+		}
+		return res;
+	}
+	
+	public static List<String> ejercicio10(BinaryTree<String> tree, String cadena) {
+		return ejercicio10_gen(tree,cadena,new ArrayList<String>());
+	}
 
+	public static void main(String[] args) {
+		List<String> lineas_n = Files2.getLines("ficheros/PI2Ej10DatosEntrada.txt");
+		System.out.println("                                                       TEST DEL EJERCICIO 10"); 
+		System.out.println("=======================================================================================================================================================");
+		for (String linea: lineas_n) {
+			BinaryTree<String> tree = BinaryTree.parse(linea.split(";")[0]);
+			System.out.println("Entrada: "+linea.split(";")[0]);
+			List<String> res = ejercicio10(tree,linea.split(";")[1]);
+			System.out.println("Salida: Los elementos mayores o iguales que "+linea.split(";")[1]+" son "+res.size()+": "+res);
+			System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------");
+		}
 	}
 
 }
--- PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio9.java
+++ PI2_CABELLOGUERRAMANUEL/src/ejercicios/Ejercicio9.java
@@ -1,10 +1,45 @@
 package ejercicios;
 
+import java.util.List;
+import java.util.function.Function;
+
+import us.lsi.common.Files2;
+import us.lsi.common.Pair;
+import us.lsi.tiposrecursivos.Tree;
+
 public class Ejercicio9 {
+	
+	public static Pair<Integer,Integer> ejercicio9_gen(Tree<Integer> tree, Pair<Integer,Integer> res) {
+		if (!tree.isEmpty() && tree.isLeaf()) {
+			res = Pair.of(Math.min(res.a, tree.getLabel()), Math.max(res.b, tree.getLabel()));
+		} else if (!tree.isEmpty() && tree.isNary()) {
+			res = Pair.of(Math.min(res.a, tree.getLabel()), Math.max(res.b, tree.getLabel()));
+			List<Tree<Integer>> children = tree.getChildren();
+			for (Tree<Integer> child : children) {
+				res = ejercicio9_gen(child,res);
+			}
+		}
+		return res;
+	}
+	
+	public static Pair<Integer,Integer> ejercicio9(Tree<Integer> tree) {
+		Pair<Integer,Integer> res = Pair.of(null, null);
+		if (!tree.isEmpty()) {
+			res = Pair.of(tree.getLabel(), tree.getLabel());
+		}
+		return ejercicio9_gen(tree, res);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> lineas_n = Files2.getLines("ficheros/PI2Ej9DatosEntrada.txt");
+		Function<String,Integer> function1 = (String cadena)-> { return Integer.valueOf(cadena); };
+		for (String linea: lineas_n) {
+			System.out.println("Árbol: "+linea);
+			Tree<String> tree_str = Tree.parse(linea);
+			Tree<Integer> tree = Ejercicio6.ejercicio6_nario(tree_str, function1);
+			Pair<Integer,Integer> res = ejercicio9(tree);
+			System.out.println("Min: "+res.a+", Max: "+res.b+"\n");
+		}
 	}
 
 }
